dc359cff0c58c010b1d11784a1a6bb32694c275b
Piotr Sienkiewicz <psienkiewicz@egnyte.com>
2015-12-29 11:46:25 +0100

    Add newly created project to git

---
+++ app/src/main/java/com/jappka/mirrorclient/GetGmailTokenActivity.java
@@ -0,0 +1,13 @@
+package com.jappka.mirrorclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetGmailTokenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_gmail_token);
+    }
+}
