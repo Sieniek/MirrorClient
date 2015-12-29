d9b28c1558678886014a5b4d7e000ffd582466cf
Piotr Sienkiewicz <psienkiewicz@egnyte.com>
2015-12-29 12:07:56 +0100

    Add basic actions for gmail auth. Add git root to idea settings.

--- app/src/main/java/com/jappka/mirrorclient/GetGmailTokenActivity.java
+++ app/src/main/java/com/jappka/mirrorclient/GetGmailTokenActivity.java
@@ -2,12 +2,29 @@ package com.jappka.mirrorclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GetGmailTokenActivity extends AppCompatActivity {
+
+    private Button requireGmailAccessButton;
+    private TextView gmailTokenLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_gmail_token);
+
+        requireGmailAccessButton = (Button) findViewById(R.id.requireGmailAccessButton);
+        gmailTokenLabel = (TextView) findViewById(R.id.gmailTokenLabel);
+
+        requireGmailAccessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gmailTokenLabel.setText("Token:\n" + Integer.toString(1234124));
+            }
+        });
     }
+
 }
